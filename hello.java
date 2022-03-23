c5e0278440d97f86b222898226da74561cd4015d
Cloud User <ec2-user@ip-172-31-21-123.ec2.internal>
2022-03-23 17:12:09 +0000

    modified

--- hello.java
+++ hello.java
@@ -11,16 +11,3 @@ public class HelloWorld {
 						}
 
 }
-package org.jacoco.examples.maven.java;
-
-public class HelloWorld {
-		
-		public String getMessage(boolean bigger) {
-					if (bigger) {
-									return "Hello Universe!";
-											} else {
-															return "Hello World!";
-																	}
-						}
-
-}
